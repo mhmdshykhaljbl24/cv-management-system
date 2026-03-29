dfc5c75c4c4af981a20c921bc5dc9a9572877a6f
Mohammad <mhmdshykhaljbl24@gmail.com>
2026-03-29 21:28:21 +0300

    fix cors production

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,10 @@ app.use(
 );
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://cv-management-system-kbdr.onrender.com",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
